6eea794c7dc43e68f0ca9200493da395e3382bdf
Archer <Archer@example.com>
2019-12-01 14:59:31 +0800

    Add test case for new code

--- codebase/src/main/java/per/archer/sort/SortUtil.java
+++ codebase/src/main/java/per/archer/sort/SortUtil.java
@@ -1,5 +1,10 @@
 package per.archer.sort;
 
 public class SortUtil {
-
+	public static void displayArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + ",");
+		}
+		System.out.println("");
+	}
 }
--- codebase/src/test/java/per/archer/sort/SortTest.java
+++ codebase/src/test/java/per/archer/sort/SortTest.java
@@ -1,13 +1,22 @@
 package per.archer.sort;
 
-import org.junit.Test;
 
-//@RunWith(SpringRunner.class)
 public class SortTest {
 	
-	@Test
-	private void quickSortTest() {
+	public static void main(String[] args) {
+		quickSortTest(getSampleDataArray());
+	}
+	
+	private static void quickSortTest(int[] data) {
+		SortUtil.displayArray(data);
 		System.out.println("Test for quick test!");
+		QuickSort.quickSort(data, 0, data.length-1);
+		SortUtil.displayArray(data);
+	}
+	
+	private static int[] getSampleDataArray() {
+		int[] arrayData = {123,32423,345,4,654,7,658,67,9789,78,98909,546,57,676,56,68,67,867,436,7,9789,78978,9,67,967,9};
+		return arrayData;
 	}
 
 }
